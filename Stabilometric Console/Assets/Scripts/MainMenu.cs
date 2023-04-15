7b729f0b55363670ef0a26829b5e76f7320de763
leoonnnnn <39069236+leoonnnnn@users.noreply.github.com>
2023-04-14 18:20:48 -0700

    Fixed Main Menu, toggled on keyboard controls

---
+++ Stabilometric Console/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // go to File-> Building Settings in Unity to check scene order. [this is dependent on the build order]
+        SceneManager.LoadScene("Runner Game"); // Alternatively, Could just call the game scene by its name (ie a string -> .LoadScene("[name]"))
+    }
+
+    public void GoToSettingsMenu()
+    {
+        SceneManager.LoadScene("Settings Menu");
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
--- Stabilometric Console/Assets/Scripts/PlayerMovement.cs
+++ Stabilometric Console/Assets/Scripts/PlayerMovement.cs
@@ -1,53 +1,6 @@
-﻿// using UnityEngine;
-// using UnityEngine.SceneManagement;
-
-// public class PlayerMovement : MonoBehaviour {
-
-//     bool alive = true;
-
-//     public float speed = 5;
-//     [SerializeField] Rigidbody rb;
-
-//     float horizontalInput;
-//     [SerializeField] float horizontalMultiplier = 2;
-
-//     public float speedIncreasePerPoint = 0.1f;
-
-//     private void FixedUpdate ()
-//     {
-//         if (!alive) return;
-
-//         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
-//         Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
-//         rb.MovePosition(rb.position + forwardMove + horizontalMove);
-//     }
-
-//     private void Update () {
-//         horizontalInput = Input.GetAxis("Horizontal");
-
-//         if (transform.position.y < -5) {
-//             Die();
-//         }
-// 	}
-
-//     public void Die ()
-//     {
-//         alive = false;
-//         // Restart the game
-//         Invoke("Restart", 2);
-//     }
-
-//     void Restart ()
-//     {
-//         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-//     }
-// }
-
+﻿// Keyboard controls
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-using System.Collections;
-using System.IO.Ports;
 
 public class PlayerMovement : MonoBehaviour {
 
@@ -61,40 +14,17 @@ public class PlayerMovement : MonoBehaviour {
 
     public float speedIncreasePerPoint = 0.1f;
 
-    SerialPort data_stream = new SerialPort("COM5", 9600);
-    public string receivedstring;
-    public GameObject test_data;
-    //public Rigidbody rb;
-    //rb = getcomponent<Rigidbody>();
-    public float sensitivity = 0.01f;
-
-    public string[] datas;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();   //added this line
-        Debug.Log("hello world");
-        data_stream.Open();
-        Debug.Log("hello world again");
-    }
-
-
     private void FixedUpdate ()
     {
         if (!alive) return;
 
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
         Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
-        rb.MovePosition(rb.position + forwardMove + horizontalMove);        
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
     }
 
     private void Update () {
-        receivedstring = data_stream.ReadLine();
-        //Debug.Log(receivedstring);
-        string[] datas = receivedstring.Split(',');  
-        Debug.Log(datas[0]);
-
-        horizontalInput = float.Parse(datas[0])/2;
+        horizontalInput = Input.GetAxis("Horizontal");
 
         if (transform.position.y < -5) {
             Die();
@@ -113,3 +43,77 @@ public class PlayerMovement : MonoBehaviour {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
+
+// TOGGLE BTWN KEYBOARD AND JOYSTICK
+
+// // Joystick controls
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+
+// using System.Collections;
+// using System.IO.Ports;
+
+// public class PlayerMovement : MonoBehaviour {
+
+//     bool alive = true;
+
+//     public float speed = 5;
+//     [SerializeField] Rigidbody rb;
+
+//     float horizontalInput;
+//     [SerializeField] float horizontalMultiplier = 2;
+
+//     public float speedIncreasePerPoint = 0.1f;
+
+//     SerialPort data_stream = new SerialPort("COM5", 9600);
+//     public string receivedstring;
+//     public GameObject test_data;
+//     //public Rigidbody rb;
+//     //rb = getcomponent<Rigidbody>();
+//     public float sensitivity = 0.01f;
+
+//     public string[] datas;
+
+//     void Start()
+//     {
+//         rb = GetComponent<Rigidbody>();   //added this line
+//         Debug.Log("hello world");
+//         data_stream.Open();
+//         Debug.Log("hello world again");
+//     }
+
+
+//     private void FixedUpdate ()
+//     {
+//         if (!alive) return;
+
+//         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+//         Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
+//         rb.MovePosition(rb.position + forwardMove + horizontalMove);        
+//     }
+
+//     private void Update () {
+//         receivedstring = data_stream.ReadLine();
+//         //Debug.Log(receivedstring);
+//         string[] datas = receivedstring.Split(',');  
+//         Debug.Log(datas[0]);
+
+//         horizontalInput = float.Parse(datas[0])/2;
+
+//         if (transform.position.y < -5) {
+//             Die();
+//         }
+// 	}
+
+//     public void Die ()
+//     {
+//         alive = false;
+//         // Restart the game
+//         Invoke("Restart", 2);
+//     }
+
+//     void Restart ()
+//     {
+//         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+//     }
+// }
