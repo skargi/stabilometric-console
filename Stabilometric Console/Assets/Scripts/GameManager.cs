b85ffffdf8a330a955b385ac89944c4fd3fdb86f
mgnho <122332058+mgnho@users.noreply.github.com>
2023-04-28 16:35:20 -0700

    Added Score Manager

--- Stabilometric Console/Assets/Scripts/GameManager.cs
+++ Stabilometric Console/Assets/Scripts/GameManager.cs
@@ -8,17 +8,9 @@ public class GameManager : MonoBehaviour {
     int score;
     public static GameManager inst;
 
-    [SerializeField] Text scoreText;
-
     [SerializeField] PlayerMovement playerMovement;
 
-    public void IncrementScore ()
-    {
-        score++;
-        scoreText.text = "SCORE: " + score;
-        // Increase the player's speed
-        playerMovement.speed += playerMovement.speedIncreasePerPoint;
-    }
+    private ScoreManager scoreManager;
 
     private void Awake ()
     {
